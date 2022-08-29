f9ab5ed698e8aa863a78a9902a5fb2e69e45a579
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-08-29 23:38:16 +0300

    ValidationAspect, FluentValidation , Autofac desteği eklendi

---
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+  public class BrandValidator : AbstractValidator<Brand>
+  {
+    public BrandValidator()
+    {
+      RuleFor(b => b.Name).MaximumLength(5);
+      RuleFor(b => b.Name).NotEmpty();
+      //RuleFor(b => b.Name).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+    }
+
+    private bool StartWithA(string arg)
+    {
+      return arg.StartsWith("A");
+    }
+  }
+}
